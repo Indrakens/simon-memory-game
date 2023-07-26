81250b219d45fc2c9cfbf4fdd7a0443ad687f445
Indrakens Zilinska <indrazilinska83@gmail.com>
2023-07-26 17:32:35 +0000

    Add show score

--- scripts/game.js
+++ scripts/game.js
@@ -7,7 +7,14 @@ let game = {
 
 function newGame() {
     game.score = 0;
+    game.currentGame = [];
+    game.playerMoves = [];
+    showScore();
+};
+
+function showScore() {
+    document.getElementById("score").innerText = game.score;
 };
 
 
-module.exports = { game, newGame };
+module.exports = { game, newGame, showScore };
