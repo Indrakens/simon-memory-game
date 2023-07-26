1666e47768ff4f98e758f37b7a91c971d3fe2156
Indrakens Zilinska <indrazilinska83@gmail.com>
2023-07-26 17:07:03 +0000

    Add newgame function

--- scripts/game.js
+++ scripts/game.js
@@ -1,9 +1,13 @@
 let game = {
+    score: 0,
     currentGame: [],
     playerMoves: [],
-    score: 0,
-    choices: ["button1", "button2", "button3", "button4"],
-}
+    choices: ["button1", "button2", "button3", "button4"]
+};
+
+function newGame() {
+    game.score = 0;
+};
 
 
-module.exports = {game};
+module.exports = { game, newGame };
